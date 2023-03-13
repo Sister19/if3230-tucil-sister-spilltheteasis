77f50f8d7914e467c3295645015b5b36bbe0d821
Timothy Stanley <timothystanleysetiawan555@gmail.com>
2023-03-13 15:52:53 +0700

    feat: cuda 🐴
    Co-Authored-By: Fikri Ranjabi <71055612+ranjabi@users.noreply.github.com>
    Co-Authored-By: Muhammad Helmi Hibatullah <helmihibatullah52@gmail.com>
    Co-Authored-By: Fikri Khoiron Fadhila <79701028+fikrikhoironn@users.noreply.github.com>

--- makefile
+++ makefile
@@ -2,17 +2,22 @@ EXECUTE_FOLDER	= bin
 ALGORITHM		= dft
 NUM_PROCESSES	= 4
 INPUT_FILE		= 128.txt
-OUTPUT_FILE		= output.txt
+OUTPUT_FILE		= output
 
 all: serial parallel
 
 open-mpi:
 	mpicc src/open-mpi/open-mpi-$(ALGORITHM).c -o $(EXECUTE_FOLDER)/open-mpi-$(ALGORITHM) -lm && \
-	time mpirun -np $(NUM_PROCESSES) $(EXECUTE_FOLDER)/open-mpi-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)
+	time mpirun -np $(NUM_PROCESSES) $(EXECUTE_FOLDER)/open-mpi-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)-open-mpi-$(ALGORITHM).txt
 
 open-mp:
 	gcc src/open-mp/open-mp-$(ALGORITHM).c -o $(EXECUTE_FOLDER)/open-mp-$(ALGORITHM) -lm -fopenmp && \
-	time $(EXECUTE_FOLDER)/open-mp-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)
+	time $(EXECUTE_FOLDER)/open-mp-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)-open-mp-$(ALGORITHM).txt
+
+cuda:
+	nvcc src/cuda/cuda-$(ALGORITHM).cu -o $(EXECUTE_FOLDER)/cuda-$(ALGORITHM) && \
+	time $(EXECUTE_FOLDER)/cuda-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)-cuda-$(ALGORITHM).txt
 
 serial:
-	gcc src/serial/c/serial-$(ALGORITHM).c -o $(OUTPUT_FOLDER)/serial-$(ALGORITHM) -lm
+	gcc src/serial/c/serial-$(ALGORITHM).c -o $(OUTPUT_FOLDER)/serial-$(ALGORITHM) -lm && \
+	time $(OUTPUT_FOLDER)/serial-$(ALGORITHM) < test_case/$(INPUT_FILE) > out/$(OUTPUT_FILE)-serial-$(ALGORITHM).txt
---
+++ src/cuda/cuda-fft.cu
@@ -0,0 +1,125 @@
+// TUBES SISTER 13520002 CUDA
+// cuda-fft.cu
+ 
+// how to run
+// > nvcc cuda-fft.cu -o cuda-fft
+// > ./cuda-fft
+ 
+// how to measure time
+// ex testcase.txt already created, create empty output.txt
+// > time ./cuda-fft < 128.txt > output.txt
+
+#include <cuComplex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_N 512
+#define BLOCK_SIZE 16
+#define CU_MPI make_cuDoubleComplex(M_PI, 0.0)
+
+struct Matrix {
+    int    size;
+    double mat[MAX_N*MAX_N];
+};
+
+struct FreqMatrix {
+    int    size;
+    cuDoubleComplex mat[MAX_N*MAX_N];
+};
+
+void readMatrix(struct Matrix *m) {
+    scanf("%d", &(m->size));
+    for (int i = 0; i < m->size; i++)
+        for (int j = 0; j < m->size; j++)
+            scanf("%lf", &(m->mat[i*m->size+j]));
+}
+
+__host__ __device__ cuDoubleComplex _cuCexp (cuDoubleComplex arg)
+{
+   cuDoubleComplex res;
+   double s, c;
+   double e = exp(arg.x);
+   sincos(arg.y, &s, &c);
+   res.x = c * e;
+   res.y = s * e;
+   return res;
+}
+
+__global__ void dft(double *d_mat, cuDoubleComplex *d_freq, int size) {
+    int k = blockIdx.x * blockDim.x + threadIdx.x;
+    int l = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (k < size && l < size) {
+        cuDoubleComplex sizeSquare      = make_cuDoubleComplex(size*size, 0.0);
+        cuDoubleComplex element         = make_cuDoubleComplex(0.0, 0.0);  
+        cuDoubleComplex var_exp         = make_cuDoubleComplex(0.0, -2.0);
+
+        cuDoubleComplex arg_even_odd    = make_cuDoubleComplex(l / (double) size, 0.0);
+        cuDoubleComplex arg_odd_even    = make_cuDoubleComplex(k / (double) size, 0.0);
+        cuDoubleComplex arg_odd_odd     = make_cuDoubleComplex((k + l) / (double) size, 0.0);
+
+        cuDoubleComplex var_even_odd    = _cuCexp(cuCmul(cuCmul(var_exp, CU_MPI), arg_even_odd));
+        cuDoubleComplex var_odd_even    = _cuCexp(cuCmul(cuCmul(var_exp, CU_MPI), arg_odd_even));
+        cuDoubleComplex var_odd_odd     = _cuCexp(cuCmul(cuCmul(var_exp, CU_MPI), arg_odd_odd));
+
+        for (int m = 0; m < size/2; m++) {
+            for (int n = 0; n < size/2; n++) {
+                cuDoubleComplex arg             = make_cuDoubleComplex(((k*m / (double) size) + (l*n / (double) size)), 0.0);
+                cuDoubleComplex exponent        = _cuCexp(cuCmul(cuCmul(var_exp, CU_MPI), arg));
+                cuDoubleComplex el_even_even    = make_cuDoubleComplex(d_mat[(2*m)*size+(2*n)],0.0);
+                cuDoubleComplex el_even_odd     = make_cuDoubleComplex(d_mat[(2*m)*size+(2*n+1)],0.0);
+                cuDoubleComplex el_odd_even     = make_cuDoubleComplex(d_mat[(2*m+1)*size+(2*n)],0.0);
+                cuDoubleComplex el_odd_odd      = make_cuDoubleComplex(d_mat[(2*m+1)*size+(2*n+1)],0.0);
+                element                         = cuCadd(element, cuCmul(el_even_even, exponent));
+                element                         = cuCadd(element, cuCmul(el_even_odd, cuCmul(var_even_odd, exponent)));
+                element                         = cuCadd(element, cuCmul(el_odd_even, cuCmul(var_odd_even, exponent)));
+                element                         = cuCadd(element, cuCmul(el_odd_odd, cuCmul(var_odd_odd, exponent)));
+            }
+        } 
+
+        d_freq[k*size+l] = cuCdiv(element, sizeSquare);   
+    }
+}
+
+
+int main(void) {
+    struct Matrix     source;
+    struct FreqMatrix freq_domain;
+    double *d_mat;
+    cuDoubleComplex *d_freq;
+
+    readMatrix(&source);
+    freq_domain.size = source.size;
+
+    cudaMalloc((void **) &d_mat, source.size * source.size * sizeof(double));
+    cudaMalloc((void **) &d_freq, source.size * source.size * sizeof(cuDoubleComplex));
+
+    cudaMemcpy(d_mat, source.mat, source.size * source.size * sizeof(double), cudaMemcpyHostToDevice);
+
+    dim3 block(source.size/BLOCK_SIZE, source.size/BLOCK_SIZE, 1);    
+    dim3 grid(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dft<<<block, grid>>>(d_mat, d_freq, source.size);
+
+    cudaMemcpy(freq_domain.mat, d_freq, source.size * source.size * sizeof(cuDoubleComplex), cudaMemcpyDeviceToHost);
+    cudaFree(d_mat);
+    cudaFree(d_freq);
+    
+    cuDoubleComplex sum = make_cuDoubleComplex(0.0, 0.0);
+    cuDoubleComplex size = make_cuDoubleComplex(source.size, 0.0);
+    for (int k = 0; k < source.size; k++) {
+        for (int l = 0; l < source.size; l++) {
+            // printf("%d %d\n", k, l);
+            cuDoubleComplex el = freq_domain.mat[k*freq_domain.size+l];
+            printf("(%lf, %lf) ", cuCreal(el), cuCimag(el));
+            sum = cuCadd(sum, el);
+        }
+        printf("\n");
+    }
+    
+    sum = cuCdiv(sum, size);
+    printf("Average : (%lf, %lf)\n", cuCreal(sum), cuCimag(sum));
+
+    cudaDeviceSynchronize();
+    return 0;
+}
